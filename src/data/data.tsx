e2d1bd31d171ae9616932de4ea75799f8016f899
nick-fournier <nichfournier@gmail.com>
2023-04-18 15:06:00 -0700

    nick to nicholas

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -65,7 +65,7 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `Nick Fournier, PhD`,
+  name: `Nicholas Fournier, PhD`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
